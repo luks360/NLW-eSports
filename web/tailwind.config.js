8137330eb504f63f69c3b4c8a791708fea9c637d
Lucas dos Santos <santoslucasantos796@gmail.com>
2024-03-04 17:42:12 -0300

    change default font

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+        sans: ['Inter', 'sans-serif']
+    },
     extend: {
         backgroundImage: {
             galaxy: "url('/background-galaxy.png')",
