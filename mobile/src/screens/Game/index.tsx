09d8ab78fc587de76550eb7064a4bf8ecb4619fe
Lucas dos Santos <santoslucasantos796@gmail.com>
2024-03-17 20:40:31 -0300

    create duo match modal

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -14,9 +14,11 @@ import { GameParams } from "../../@types/navigation"
 import { Heading } from "../../components/Heading"
 import { Background } from "../../components/Background"
 import { DuoCard, DuoCardProps } from "../../components/DuoCard"
+import { DuoMatch } from "../../components/DuoMatch"
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([])
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('')
 
   const navigation = useNavigation()
   const route = useRoute()
@@ -27,10 +29,19 @@ export function Game() {
     navigation.goBack()
   }
 
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://${address}:3333/ads/${adsId}/discord`)
+      .then(response => response.json())
+      .then(data => setDiscordDuoSelected(data.discord))
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   useEffect(() => {
     fetch(`http://${address}:3333/games/${game.id}/ads`)
-      .then((response) => response.json())
-      .then((data) => setDuos(data))
+      .then(response => response.json())
+      .then(data => setDuos(data))
       .catch((error) => {
         console.log(error)
       })
@@ -65,7 +76,7 @@ export function Game() {
           data={duos}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <DuoCard data={item} onConnect={() => {}} />
+            <DuoCard data={item} onConnect={() => getDiscordUser(item.id)} />
           )}
           horizontal
           style={styles.containerList}
@@ -78,6 +89,12 @@ export function Game() {
               Não há anúncios publicados ainda.
             </Text>
           )}
+        />
+
+        <DuoMatch 
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
         />
       </SafeAreaView>
     </Background>
